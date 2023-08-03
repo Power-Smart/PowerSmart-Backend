c68083b6f25a3b92a61461e10f68828e545b35dc
kavishka-sulakshana <kss20001218@gmail.com>
2023-08-03 18:39:35 +0530

    cloud database connection

--- config/db.config.js
+++ config/db.config.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbConfig = {
-    HOST: "localhost",
-    PORT: 5432,
+    HOST: process.env.DB_HOST,
+    PORT: process.env.DB_PORT,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
     DB: process.env.DB_NAME,
