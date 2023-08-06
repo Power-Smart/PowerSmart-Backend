20f1b3ea81ab4b8ea72dc788d85e3a5949cc80b0
kavishka-sulakshana <kss20001218@gmail.com>
2023-08-06 20:46:09 +0530

    update customer profile functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,6 @@
 import Customer from "../models/customer.model.js";
 import User from "../models/user.model.js";
 
-
 export const getUser = async (req, res) => {
     let id = req.params.id;
     try {
@@ -12,9 +11,26 @@ export const getUser = async (req, res) => {
     }
 };
 
-export const saveProfile = async (req,res) => {
+export const updateCustomerProfile = async (req, res) => {
+    const id = req.params.id;
+    const { name, tel_no, address } = req.body;
+    try {
+        const customer = await Customer.findByPk(id);
+        customer.tel_no = [tel_no.split(",")];
+        customer.address = address;
+        await customer.save();
+        const user = await User.findByPk(id);
+        user.name = name;
+        await user.save();
+        res.status(200).send(customer);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error updating profile");
+    }
+};
+
+export const saveProfile = async (req, res) => {
     // console.log(req.params.id);
     // console.log(req.file);
     res.status(200).send();
-}
-
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,15 @@
 import Express from "express";
-import { getUser, saveProfile } from "../controllers/user.controller.js";
+import {
+    getUser,
+    saveProfile,
+    updateCustomerProfile,
+} from "../controllers/user.controller.js";
 import { profileUpload } from "../config/multer.config.js";
-
 
 const router = Express.Router();
 
 router.get("/:id", getUser);
+router.patch("/customer_profile/:id", updateCustomerProfile);
 router.post("/saveProfilePic/:id", profileUpload, saveProfile);
 
 export default router;
