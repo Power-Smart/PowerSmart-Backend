b7e29312f8164172c223b2e4c46206bc45013b34
kavishka-sulakshana <kss20001218@gmail.com>
2023-08-16 14:35:07 +0530

    device controller successfully done

--- controllers/devices.controller.js
+++ controllers/devices.controller.js
@@ -1,5 +1,7 @@
 import Device from "../models/device.model.js";
 import DeviceSwitching from "../models/deviceSwitching.model.js";
+import db from "../models/index.js";
+import _ from "lodash";
 
 export const getDevicesData = async (req, res) => {
     try {
@@ -53,7 +55,43 @@ export const getDevice = async (req, res) => {
 export const toggleDevice = async (req, res) => {
     const { deviceID, status } = req.params;
     try {
-        res.status(200).json({ message: "Device toggled" });
+        const [relaySocket, metadata] = await db.query(`SELECT relay_unit_id, socket FROM devices WHERE device_id=${deviceID}`);
+        console.log("relaySocket : ", relaySocket);
+        const wsDataToSendOnce = {
+            switchingScheme: {
+                [relaySocket[0].relay_unit_id]: {
+                    [relaySocket[0].socket]: status
+                }
+            }
+        };
+
+        console.log("wsDataToSendOnce : ", wsDataToSendOnce);
+
+        fetch('http://20.253.48.86:4001/relayswitch', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(wsDataToSendOnce)
+        })
+            .then((response) => response.json())
+            .then(async (data) => {
+                console.log("data : ", data)
+                if (_.isEmpty(data.notFoundRelays)) {
+                    await DeviceSwitching.update({
+                        switch_status: status,
+                    }, {
+                        where: {
+                            device_id: deviceID,
+                            status: "active",
+                        }
+                    });
+                } else {
+                    throw new Error("Relay not found");
+                }
+                res.status(200).json({ message: "Device toggled" });
+            })
+            .catch((err) => res.status(500).json({ error: err.message }));
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
