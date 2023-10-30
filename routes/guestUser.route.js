766eff025eacd62c60e35246d91f0f50d93495f0
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-10-30 16:37:35 +0530

    guset user model and guest user suggestion

--- routes/guestUser.route.js
+++ routes/guestUser.route.js
@@ -1,9 +1,19 @@
 import Express from "express";
-import { addGuestUserSuggest,getGuestUserSuggest } from "../controllers/guestuser.controller.js";
+import { addGuestUser,takeActionAndNotWant,sendSMSWhenAcceptGusetUserRequest,sendSMSWhenRejectGusetUserRequest,addNewGuestUserSuggest,getAllGuestUser,banAndUnbanGuestUser,getAllGuestSuggest } from "../controllers/guestuser.controller.js";
 
 const router = Express.Router();
 
-router.post("/", addGuestUserSuggest);
-router.get("/:customerID", getGuestUserSuggest);
+router.post("/", addGuestUser);
+router.post("/addSuggest", addNewGuestUserSuggest);
+router.post("/banAndUnbanGuestUser", banAndUnbanGuestUser);
+router.get("/allSuggest/:customerID", getAllGuestSuggest);
+router.post("/takeActionAndNotWant", takeActionAndNotWant);
+router.get("/allGuestUser/:customerID", getAllGuestUser);
+router.post("/sendSMSWhenAcceptGusetUserRequest", sendSMSWhenAcceptGusetUserRequest);
+router.post("/sendSMSWhenRejectGusetUserRequest", sendSMSWhenRejectGusetUserRequest);
+
+
+// router.get("/allGuestUser/:customerID", getGuestUserSuggest);
+
 
 export default router;
