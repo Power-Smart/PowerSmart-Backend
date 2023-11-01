b79996c5217b37a7b1bd61e3021630b3bbb256f7
lahiru1115 <lahirudissanayake15@gmail.com>
2023-11-01 06:42:57 +0530

    create tech support controller

---
+++ controllers/adminTechSupport.controller.js
@@ -0,0 +1,31 @@
+import TechSupport from "../models/techSupport.model.js";
+import User from "../models/user.model.js";
+
+export const getTechSupportView = async (req, res) => {
+    try {
+        const results = await TechSupport.findAll({
+            include: {
+                model: User,
+                as: 'user',  // Assuming you have defined 'as' property in your association in Sequelize
+                attributes: ['user_id', 'first_name', 'last_name', 'email'],
+            },
+            order: [[{ model: User, as: 'user' }, 'user_id', 'ASC']],
+        });
+
+        // Extract the user data from the nested JSON and send it back
+        const techSupportWithUserData = results.map(techSupport => {
+            return {
+                user_id: techSupport.user.user_id,
+                first_name: techSupport.user.first_name,
+                last_name: techSupport.user.last_name,
+                email: techSupport.user.email,
+                tel_no: techSupport.tel_no,
+                is_banned: techSupport.is_banned
+            };
+        });
+
+        res.status(200).json(techSupportWithUserData);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
