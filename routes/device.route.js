24ce3121a0b22cee732b8e1dc72dea0c1a4d5853
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-11-02 12:44:16 +0530

    done

--- routes/device.route.js
+++ routes/device.route.js
@@ -1,10 +1,11 @@
 import Express from "express";
 import { getDevicesData, getDevice, toggleDevice, deviceSwitch } from "../controllers/devices.controller.js";
+import { switchDevicesManually } from "../controllers/control.controller.js";
 
 const router = Express.Router();
 
 router.post("/scheduledSwitch", deviceSwitch)
-router.get("/toggle/:deviceID/:status", toggleDevice);
+router.get("/toggle/:deviceID/:status", switchDevicesManually);
 router.get("/:userID/:roomID", getDevicesData);
 router.get("/:userID/:roomID/:deviceID", getDevice);
 
